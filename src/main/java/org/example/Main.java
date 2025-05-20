2a110e048459724116a6101db8f87a4db7cf1e76
eeuunn <haspring@dsm.hs.kr>
2025-05-20 14:47:15 +0900

    문제 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static int countWaysToMakeSum(int[] coins, int targetSum) {
@@ -16,9 +18,22 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] coins = {2, 5, 3, 6};
-        int sum = 10;
+        Scanner scanner = new Scanner(System.in);
+
+        // 합계 입력
+        System.out.print("합계를 입력하세요: ");
+        int sum = scanner.nextInt();
+
+        // 동전 개수 및 동전 종류 입력
+        System.out.print("동전 종류의 개수를 입력하세요: ");
+        int n = scanner.nextInt();
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
         int result = countWaysToMakeSum(coins, sum);
-        System.out.println("방법의 수: " + result);
+        System.out.println("만들 수 있는 조합의 수: " + result);
     }
 }
